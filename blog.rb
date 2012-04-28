2659b15fca9d99e05b3d7a6fd428e21ff1b38e98
Jonathan Raiman <jonathanraiman@gmail.com>
2012-04-28 02:16:05 -0700

    changed language of path for uploading files

--- blog.rb
+++ blog.rb
@@ -470,7 +470,7 @@ post '/upload' do
   n = Image.new
   n.filename = params[:image][:filename]
   n.created_at = Time.now
-  FileUtils.copy(params[:image][:tempfile].path, "./tmp/uploads/"+params[:image][:filename])
+  FileUtils.copy(params[:image][:tempfile].path, "./tmp/uploads/")#+params[:image][:filename]
   n.url = "/tmp/uploads/#{params[:image][:filename]}"
   if n.save 
     redirect '/'
