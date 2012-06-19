5337ad104dcac87c99189d8d16cff026006c4c8a
Jonathan Raiman <jonathanraiman@gmail.com>
2012-06-19 23:13:48 +0200

    added ILIKE Statement

--- blog.rb
+++ blog.rb
@@ -421,7 +421,7 @@ get '/search.json' do
   elsif params[:term] == "CHEAP"
     books = Book.all(:price.lt => 10, :order=> :price)
   else
-    books = Book.all(:conditions => ["name LIKE ?","%#{params[:term]}%"])+Book.all(:conditions => ["author LIKE ?","%#{params[:term]}%"])+Book.all(:conditions => ["description LIKE ?","%#{params[:term]}%"])
+    books = Book.all(:conditions => ["name ILIKE ?","%#{params[:term]}%"])+Book.all(:conditions => ["author ILIKE ?","%#{params[:term]}%"])+Book.all(:conditions => ["description ILIKE ?","%#{params[:term]}%"])
     #books = Book.all(:name.like => "%#{params[:term]}%")+Book.all(:author.like => "%#{params[:term]}%")+Book.all(:description.like => "%#{params[:term]}%")
   end
   response = []
